c086b0baacb3c6c4b831cc1f7cdb4d3407aec55e
Priya <priyahoney1824@gmail.com>
2022-04-22 10:39:25 +0000

    ingress added

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+# Uninstall Traefik
+helm uninstall traefik
+
+# Uninstall Applications
+kubectl delete -f .
---
+++ install.sh
@@ -0,0 +1,8 @@
+# Install Traefik
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm install traefik traefik/traefik
+
+
+# Install Applications
+kubectl apply -f .
